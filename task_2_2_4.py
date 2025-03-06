f4917789985002c5014a953451edf612c9ce82f4
Demid <example@test.ru>
2025-03-06 13:25:25 +0300

    fixc 2.2_4

--- task_2_2_4.py
+++ task_2_2_4.py
@@ -1,5 +1,8 @@
 # Задача 2.2.4
 
+def convert_temp(F):
+    temp_C = (F - 32) * 5 / 9
+    return f'{F} градусов по Фаренгейту {round(temp_C, 2)} по Цельсию'
+
 F = 100
-C = (F - 32) * 5 / 9
-print(f'100 градусов по Фаренгейту равняется {round(C, 2)} по Цельсию')
+print(convert_temp(F))
