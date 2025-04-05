7ab96c04e3be25c4e984e207d7a9e6a29ee42c83
Demid <example@test.ru>
2025-04-05 23:03:23 +0300

    fix_new test_4_3

--- project_4_3/tests/test_items.py
+++ project_4_3/tests/test_items.py
@@ -129,6 +129,23 @@ class TestItems():
         # assert data["count"] - 1 == len(data["data"]), "'count' - 1 должен быть равен кол-ву элементов в списке"
         assert limit == len(data["data"]), "Кол-во элементов не совпадает в лимитом"
 
+    # ?skip = 0 & limit = 5 (1 page)
+    # ?skip = 5 & limit = 5 (2 page)
+    # ?skip = 40 & limit = 5 (9 page)
+    def test_scope_page(self, auth_session):
+        params_page_1 = {"skip": 0, "limit": 5}
+        params_page_2 = {"skip": 5, "limit": 5}
+        params_page_9 = {"skip": 40, "limit": 5}
+        get_responce = auth_session.get(f"{self.endpoint}", params=params_page_1)
+        assert get_responce.status_code == 200, (f"Ошибка при получении данных: "
+                                                 f"{get_responce.status_code}, тело-ответа: {get_responce.text}")
+
+        data = get_responce.json()
+        if data["data"][0]["title"] == 'Government' and data["data"][4]["title"] == 'Enter':
+            print(f"Пагинация на 1-ой странице корректная: ", data)
+        else:
+            print(f"Пагинация на 1-ой странице некорректная: ", data)
+
     def test_get_items_not_auth(self):
         skip = 5
         limit = random.randint(11, 20)
@@ -205,7 +222,20 @@ class TestItems():
         else:
             print(f"\n Нет данных по title: {responce.status_code}, {responce.text}")
 
+    # Апи в данном случае нужно дорабатывать, так как в сваггере нет дополнительных параметров по типу:
+    # sort_py, order
+    # После прикручивать автотест
     def test_sort_by_title(self, auth_session):
+        get_responce = auth_session.get(self.endpoint)
+        assert get_responce.status_code == 200, (f"Ошибка при получении данных: "
+                                                 f"{get_responce.status_code}, тело-ответа: {get_responce.text}")
+
+        items = get_responce.json()
+        titles = [item["title"] for item in items["data"]]
+        desc_titles = sorted(titles, reverse=True)
+        asc_titles = sorted(titles)
+        print(asc_titles)
+
 
 
     # PUT /items/{id} — полное обновление элемента.
@@ -215,4 +245,126 @@ class TestItems():
                                                   f"{post_responce.status_code}, тело ответа: {post_responce.text}")
 
         item_id = post_responce.json().get("id")
+        data_post = post_responce.json()
+        print(f"\n {data_post}")
+
         put_responce = auth_session.put(f"{self.endpoint}{item_id}", json=item_data_update)
+        assert put_responce.status_code == 200, (f"Неверный статус код при изменении объекта: "
+                                                 f"{post_responce.status_code}, тело ответа: {post_responce.text}")
+
+        data_put = put_responce.json()
+        assert data_post["id"] == data_put["id"], "id не совпадает"
+        assert data_post["owner_id"] == data_put["owner_id"], "id владельца не совпадает"
+        assert data_put["title"] == item_data_update["title"], "Данные в поле title не обновлены"
+        assert data_put["description"] == item_data_update["description"], "Данные в поле description не обновлены"
+        print(f"\n {data_put}")
+
+
+    # Статус ответ приходит 401, вместо 422 (как заявлено в документации)
+    def test_update_not_auth(self, auth_session, item_data, item_data_update):
+        post_responce = auth_session.post(f"{self.endpoint}", json=item_data)
+        assert post_responce.status_code == 200, (f"Неверный статус код при создании объекта: "
+                                                  f"{post_responce.status_code}, тело ответа: {post_responce.text}")
+
+        item_id = post_responce.json().get("id")
+        data_post = post_responce.json()
+        print(data_post)
+
+        put_responce = requests.put(f"{self.endpoint}{item_id}", json=item_data_update)
+        assert put_responce.status_code == 422, (f"Неверный статус без сессии: "
+                                                 f"{put_responce.status_code}, тело ответа: {put_responce.text}")
+
+        assert data_post["title"] == item_data["title"], "Данные в поле title не совпадают"
+        assert data_post["description"] == item_data["description"], "Данные в поле description не совпадают"
+        assert "detail" in put_responce.json(), f"Сверить тело ответа с документацией: {put_responce.text}"
+
+    def test_update_not_valid_id(self, auth_session, item_data_update):
+        not_valid_id = 2324
+        put_responce = auth_session.put(f"{self.endpoint}{not_valid_id}", json=item_data_update)
+        assert put_responce.status_code == 422, (f"Неверный статус код. Ожидается 422, "
+                                                 f"получен {put_responce.status_code}, "
+                                                 f"тело ответа: {put_responce.text}")
+        assert "detail" in put_responce.json(), "Тело ответа не содержит сообщение об ошибке"
+
+    def test_update_not_valid_data(self, auth_session, item_data):
+        post_responce = auth_session.post(f"{self.endpoint}", json=item_data)
+        assert post_responce.status_code == 200, (f"Неверный статус код при создании объекта: "
+                                                  f"{post_responce.status_code}, тело ответа: {post_responce.text}")
+
+        item_id = post_responce.json().get("id")
+        data = post_responce.json()
+
+        not_valid_data = {
+            "title": "",
+            "description": "V"
+        }
+
+        put_responce = auth_session.put(f"{self.endpoint}{item_id}", json=not_valid_data)
+        assert put_responce.status_code == 422, (f"Неверный статус код с невалидным телом запроса "
+                                                 f"{put_responce.status_code}, "
+                                                 f"тело ответа: {put_responce.text}")
+
+        assert data["title"] == item_data["title"], f"Данные в поле title измененены: {data["title"]}"
+        assert data["description"] == item_data["description"], (f"Данные в поле description измененны: "
+                                                                 f"{data["description"]}")
+
+    # DELETE /items/{id} — удаление объекта.
+    def test_delete_id(self, auth_session, item_data):
+        post_responce = auth_session.post(f"{self.endpoint}", json=item_data)
+        assert post_responce.status_code == 200, (f"Неверный статус код при создании объекта: "
+                                                  f"{post_responce.status_code}, "
+                                                  f"тело ответа: {post_responce.text}")
+
+        item_id = post_responce.json().get("id")
+
+        delete_responce = auth_session.delete(f"{self.endpoint}{item_id}")
+        assert delete_responce.status_code == 200, (f"Неверный статус код при удалении объекта: "
+                                                    f"{delete_responce.status_code}, "
+                                                    f"тело ответа: {delete_responce.text}")
+
+        assert "message" in delete_responce.json(), "Тело ответа не содержит сообщения об удалении"
+
+        get_responce_id = auth_session.get(f"{self.endpoint}{item_id}")
+        assert get_responce_id.status_code == 404, (f"Неверный статус код при поиске удаленного объекта: "
+                                                    f"{get_responce_id.status_code},"
+                                                    f"тело ответа: {get_responce_id.text}")
+
+    def test_two_delete_id(self, auth_session, item_data):
+        post_responce = auth_session.post(f"{self.endpoint}", json=item_data)
+        assert post_responce.status_code == 200, (f"Неверный статус код при создании объекта: "
+                                                  f"{post_responce.status_code}, "
+                                                  f"тело ответа: {post_responce.text}")
+
+        item_id = post_responce.json().get("id")
+        delete_responce = auth_session.delete(f"{self.endpoint}{item_id}")
+        assert delete_responce.status_code == 200, (f"Неверный статус код при удалении объекта: "
+                                                    f"{delete_responce.status_code}, "
+                                                    f"тело ответа: {delete_responce.text}")
+
+        delete_responce = auth_session.delete(f"{self.endpoint}{item_id}")
+        assert delete_responce.status_code == 404, (f"Неверный статус код при удалении объекта: "
+                                                    f"{delete_responce.status_code}, "
+                                                    f"тело ответа: {delete_responce.text}")
+        assert "detail" in delete_responce.json(), "Тело ответа содержит другой ответ"
+
+    def test_delete_non_existent_id(self, auth_session):
+        id = "6e04c8fe-3238-4a11-bce6-a368b0"
+        delete_responce = auth_session.delete(f"{self.endpoint}{id}")
+        assert delete_responce.status_code == 422, (f"Неверный статус код при удалении объекта: "
+                                                    f"{delete_responce.status_code}, "
+                                                    f"тело ответа: {delete_responce.text}")
+        assert "detail" in delete_responce.json(), "Тело ответа содержит другую структуру"
+
+    def test_delete_not_auth(self, auth_session, item_data):
+        post_responce = auth_session.post(f"{self.endpoint}", json=item_data)
+        assert post_responce.status_code == 200, (f"Неверный статус код при создании объекта: "
+                                                  f"{post_responce.status_code}, "
+                                                  f"тело ответа: {post_responce.text}")
+
+        item_id = post_responce.json().get("id")
+        delete_responce = requests.delete(f"{self.endpoint}{item_id}")
+        assert delete_responce.status_code == 401, (f"Неверный статус код при удалении объекта: "
+                                                    f"{delete_responce.status_code}, "
+                                                    f"тело ответа: {delete_responce.text}")
+        data = delete_responce.json()
+        assert data["detail"] == "Not authenticated", "Приходит другой ответ в теле"
